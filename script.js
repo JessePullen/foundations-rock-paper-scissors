d8437e1401b52681c15c05f8be835bcc7bc0a049
JessePullen <jessepullen@icloud.com>
2022-05-18 20:36:26 +1000

    Finish playGame function and finish project

--- script.js
+++ script.js
@@ -1,67 +1,91 @@
-/*
-function playRound
-    call playRound
-    play 5 round game using for loop
-    keep score and report winner or loser at the end
-
--fix issue where playRound() returns undefined if the user does not enter a valid input.
-
-*/
-
-
 //Randomises a choice for the computer opponent and returns the result
 function computerPlay() {
-    //Variable choice becomes a random float between 0 and 2, Math.floor rounds down to an integer
-    let computerSelection = Math.floor(Math.random() * 3);
-    //Assigns a selection based on 3 numbers
-    if (computerSelection == 2) {
+    let selection = Math.floor(Math.random() * 3);
+
+    if (selection == 2) {
         return 'rock';
-    } else if (computerSelection == 1) {
+    } else if (selection == 1) {
         return 'paper';
     } else {
         return 'scissors';
     }
 }
-//Takes user input for their selection and checks that the answer is valid, if not the function is called again to re-prompt the user
-function userPlay() {
-    let playerSelection = prompt('Please enter: rock, paper or scissors.').toLowerCase();
-    
-    if (playerSelection == 'rock' || playerSelection == 'paper' || playerSelection == 'scissors') {
-        return playerSelection;
+
+//Takes player input for their selection and checks that the answer is valid, if not the function is called again to re-prompt the player
+function playerPlay() {
+    let selection = prompt('Please enter: rock, paper or scissors.').toLowerCase();
+
+    if (selection == 'rock' || selection == 'paper' || selection == 'scissors') {
+        return selection;
     } else {
-        userPlay();
+        playerPlay();
     }
 }
 
-//Takes user and computer selection from previous functions and checks who wins the round based on rules of the game
-function playRound(playerSelection, computerSelection) {
-    //Calls functions to determine the selection for the user and computer
-    playerSelection = userPlay();
-    computerSelection = computerPlay();
-    //Prints selections for the round
+//Takes player and computer selection using previous functions and returns who wins the round based on rules of the game
+function playRound() {
+    let playerSelection = playerPlay();
+    let computerSelection = computerPlay();
+
     console.log(`You chose: ${playerSelection}, Computer chose: ${computerSelection}`);
-    //Checks who wins based on scissors, paper, rock game rules and prints the winner
+
     if (playerSelection == computerSelection) {
         console.log('Draw');
     } else if (playerSelection == 'rock') {
         if (computerSelection == 'paper') {
             console.log('Computer wins, paper beats rock.');
+            return 'computerWin';
         } else {
             console.log('You win, rock beats scissors.');
+            return 'playerWin';
         }
     } else if (playerSelection == 'paper') {
         if (computerSelection == 'scissors') {
             console.log('Computer wins, scissors beats paper.');
+            return 'computerWin';
         } else {
             console.log('You win, paper beats rock.');
+            return 'playerWin';
         }
     } else if (playerSelection == 'scissors') {
         if (computerSelection == 'rock') {
             console.log('Computer wins, rock beats scissors.');
+            return 'computerWin';
         } else {
             console.log('You win, scissors beats paper.');
+            return 'playerWin';
         }
     }
 }
 
-playRound();
+//Plays 5 rounds and handles scoring
+function playGame() {
+    let rounds = 0;
+    let playerTally = 0;
+    let computerTally = 0;
+    
+    for (let i = 0; i < 5; i++) {
+        let roundResult = playRound();
+        rounds++;
+
+        if (roundResult == 'computerWin') {
+            computerTally++;
+        } else if (roundResult == 'playerWin') {
+            playerTally++;
+        }
+
+        console.log(`Rounds Played: ${rounds}\n` + 
+        `Computer Score: ${computerTally}\n` +
+        `Player Score: ${playerTally}`)
+    }
+
+    if (computerTally > playerTally) {
+        console.log('Computer wins this game');
+    } else if (computerTally < playerTally) {
+        console.log('You win this game');
+    } else {
+        console.log('This game ends in a draw');
+    }
+}
+
+playGame();
