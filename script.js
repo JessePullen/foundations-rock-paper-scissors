9694216291747687f933c955c3dddc131a2d8f13
JessePullen <jessepullen@icloud.com>
2022-05-18 13:32:29 +1000

    Finish playRound function and add comments

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 /*
-function playRound(playerSelection, computerSelection)
-    -return string that declares winner of the round
-    =eg "You Lose! Paper beats Rock"
-    -case insensitive input for playerSelection
-
 function playRound
     call playRound
     play 5 round game using for loop
     keep score and report winner or loser at the end
+
+-fix issue where playRound() returns undefined if the user does not enter a valid input.
+
 */
 
 
-//Randomises a choice for the computer opponent and returns the result.
+//Randomises a choice for the computer opponent and returns the result
 function computerPlay() {
-    // variable choice becomes a random float between 0 and 2, Math.floor rounds down to an integer
+    //Variable choice becomes a random float between 0 and 2, Math.floor rounds down to an integer
     let computerSelection = Math.floor(Math.random() * 3);
+    //Assigns a selection based on 3 numbers
     if (computerSelection == 2) {
         return 'rock';
     } else if (computerSelection == 1) {
@@ -23,23 +22,46 @@ function computerPlay() {
         return 'scissors';
     }
 }
-//Takes user input and checks that the answer is valid, if not the function is called again to re-prompt the user.
+//Takes user input for their selection and checks that the answer is valid, if not the function is called again to re-prompt the user
 function userPlay() {
     let playerSelection = prompt('Please enter: rock, paper or scissors.').toLowerCase();
     
     if (playerSelection == 'rock' || playerSelection == 'paper' || playerSelection == 'scissors') {
-        console.log(playerSelection);
+        return playerSelection;
     } else {
         userPlay();
     }
 }
 
-//incomplete
+//Takes user and computer selection from previous functions and checks who wins the round based on rules of the game
 function playRound(playerSelection, computerSelection) {
+    //Calls functions to determine the selection for the user and computer
     playerSelection = userPlay();
     computerSelection = computerPlay();
-
-    console.log(playerSelection);
+    //Prints selections for the round
+    console.log(`You chose: ${playerSelection}, Computer chose: ${computerSelection}`);
+    //Checks who wins based on scissors, paper, rock game rules and prints the winner
+    if (playerSelection == computerSelection) {
+        console.log('Draw');
+    } else if (playerSelection == 'rock') {
+        if (computerSelection == 'paper') {
+            console.log('Computer wins, paper beats rock.');
+        } else {
+            console.log('You win, rock beats scissors.');
+        }
+    } else if (playerSelection == 'paper') {
+        if (computerSelection == 'scissors') {
+            console.log('Computer wins, scissors beats paper.');
+        } else {
+            console.log('You win, paper beats rock.');
+        }
+    } else if (playerSelection == 'scissors') {
+        if (computerSelection == 'rock') {
+            console.log('Computer wins, rock beats scissors.');
+        } else {
+            console.log('You win, scissors beats paper.');
+        }
+    }
 }
 
-userPlay();
+playRound();
