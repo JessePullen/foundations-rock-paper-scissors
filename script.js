eb14c1b0215a4b6cd3d499da87d644e2cf174fd8
JessePullen <jessepullen@icloud.com>
2022-05-25 16:05:15 +1000

    Remove round function and add functionality to new function declareWinner which also declares winner

--- script.js
+++ script.js
@@ -13,24 +13,6 @@ let playerTally = 0;
 let computerTally = 0;
 let winner;
 
-
-
-rock.addEventListener('click', () => {
-    playerSelection = 'rock';
-    playRound(playerSelection);
-    updateRounds()
-});
-paper.addEventListener('click', () => {
-    playerSelection = 'paper';
-    playRound(playerSelection);
-    updateRounds()
-});
-scissors.addEventListener('click', () => {
-    playerSelection = 'scissors';
-    playRound(playerSelection);
-    updateRounds()
-});
-
 //Randomises a choice for the computer opponent and returns the result
 function computerPlay() {
     let selection = Math.floor(Math.random() * 3);
@@ -55,67 +37,58 @@ function playRound(playerSelection) {
     } else if (playerSelection == 'rock') {
         if (computerSelection == 'paper') {
             results.textContent = 'Computer wins, paper beats rock.';
-            return 'computerWin';
+            computerTally++
         } else {
             results.textContent = 'You win, rock beats scissors.';
-            return 'playerWin';
+            playerTally++
         }
     } else if (playerSelection == 'paper') {
         if (computerSelection == 'scissors') {
             results.textContent = 'Computer wins, scissors beats paper.';
-            return 'computerWin';
+            computerTally++
         } else {
             results.textContent = 'You win, paper beats rock.';
-            return 'playerWin';
+            playerTally++
         }
     } else if (playerSelection == 'scissors') {
         if (computerSelection == 'rock') {
             results.textContent = 'Computer wins, rock beats scissors.';
-            return 'computerWin';
+            computerTally++
         } else {
             results.textContent = 'You win, scissors beats paper.';
-            return 'playerWin';
+            playerTally++
         }
     }
 }
 
-// Plays 5 round and handles scoring
-// function playGame() {
-//     for (let i = 0; i < 5; i++) {
-//         let roundResult = playRound();
-//         round++;
-
-//         if (roundResult == 'computerWin') {
-//             computerTally++;
-//         } else if (roundResult == 'playerWin') {
-//             playerTally++;
-//         }
-
-//         console.log(`Rounds Played: ${round}\n` +
-//             `Computer Score: ${computerTally}\n` +
-//             `Player Score: ${playerTally}`)
-//     }
-
-//     if (computerTally > playerTally) {
-//         console.log('Computer wins this game');
-//     } else if (computerTally < playerTally) {
-//         console.log('You win this game');
-//     } else {
-//         console.log('This game ends in a draw');
-//     }
-// }
-
-function updateRounds() {
-    // round++
-    // if (round = 5) {
-    //     round = 0;
-    // }
-    // rounds.textContent = `Rounds: ${round}`;
+function declareWinner() {
     round++
     console.log(round);
     rounds.textContent = `Round: ${round}`;
-    if (round == 5) {
-        console.log('end of game');
+    console.log(`computer: ${computerTally}, player: ${playerTally}`);
+    if (playerTally == 5) {
+        console.log('player wins');
+        round = 0;
+    } else if (computerTally == 5) {
+        console.log('computer wins');
         round = 0;
     }
 }
+
+function playGame() {
+    playRound(playerSelection)
+    declareWinner();
+}
+
+rock.addEventListener('click', () => {
+    playerSelection = 'rock';
+    playGame(playerSelection);
+});
+paper.addEventListener('click', () => {
+    playerSelection = 'paper';
+    playGame(playerSelection);
+});
+scissors.addEventListener('click', () => {
+    playerSelection = 'scissors';
+    playGame(playerSelection);
+});
